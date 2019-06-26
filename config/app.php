b74b36f56ab0e0ea79f3b843b8b6d49cec430158
EC2 Default User <ec2-user@ip-172-31-88-238.ec2.internal>
2019-06-26 09:25:18 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -139,6 +139,8 @@ return [
         
         // 追加
         Collective\Html\HtmlServiceProvider::class,
+        // 追加
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -199,6 +201,8 @@ return [
          // 追加
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        // 追加
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
